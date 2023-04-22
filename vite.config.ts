8e29bfd60c095f5c57e83a6ef5d8d04801f07e86
Ilya Semenov <semenov@inetss.com>
2023-04-22 14:37:22 +0300

    выкладка на github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import pages from "vite-plugin-pages"
 import tsconfig_paths from "vite-tsconfig-paths"
 
 export default defineConfig({
+	base: process.env.BASE_URL,
 	plugins: [tsconfig_paths({ loose: true }), vue(), pages()],
 	resolve: {
 		alias: {
